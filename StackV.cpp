61c5b33a9593e53309f7ff86fb79bb935619a6b9
geewizz246 <geewizz246@users.noreply.github.com>
2018-03-19 10:41:43 -0400

    Added implementations for stackv

--- StackV.cpp
+++ StackV.cpp
@@ -1,1 +1,27 @@
 #include "StackV.h"
+
+int Stack::size()
+{
+	return data.size();
+}
+
+void Stack::push(int val)
+{
+	data.push_back(val);
+}
+
+void Stack::pop()
+{
+	data.pop_back();
+}
+
+int Stack::top()
+{
+	int val = data.back();
+	return val;
+}
+
+void Stack::clear()
+{
+	data.clear();
+}
